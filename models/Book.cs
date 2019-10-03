6d239b180bec9175af38826d7ac868741a1d861c
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-10-03 15:09:22 -0600

    book model

---
+++ models/Book.cs
@@ -0,0 +1,29 @@
+namespace library.models
+{
+    class Book
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public Genre Genre { get; private set; }
+        public int Pages { get; private set; }
+
+        public Book(string title, string author, Genre genre, int pages)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Pages = pages;
+        }
+    }
+
+    enum Genre
+    {
+        Adventure,
+        Romance,
+        SciFi,
+        Fantasy,
+        Horror,
+        Mystery,
+        Nonfiction
+    }
+}
